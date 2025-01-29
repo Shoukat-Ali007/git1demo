f1cef47e5187be4159bf08671e4ccb8c7f632012
Shoukat Ali <shoukatbhatt54@gmail.com>
2025-01-29 09:18:31 -0800

    ignore errors

--- demo1.dart
+++ demo1.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+// ignore: camel_case_types
 class demoScreen extends StatefulWidget{
+  const demoScreen({super.key});
+
   @override
   State<demoScreen> createState() => _demoScreenState();
 }
 
+// ignore: camel_case_types
 class _demoScreenState extends State<demoScreen> {
   @override
   Widget build(BuildContext context) {
